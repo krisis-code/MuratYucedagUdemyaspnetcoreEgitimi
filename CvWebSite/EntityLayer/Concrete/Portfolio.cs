2e0772786309ca14243fba45cf3136ef97c27dad
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-05-19 11:48:47 +0300

    Entity layer experince,portfolio,testimonial entityleri oluşturuldu

---
+++ CvWebSite/EntityLayer/Concrete/Portfolio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Portfolio
+    {
+        public int PortfolioId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? ImageUrl { get; set; }
+    }
+}
---
+++ CvWebSite/EntityLayer/Concrete/Skill.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Skill
+    {
+        //Skill bölümünün kimliği
+        public int SkillId { get; set; }
+
+        //Skill bölümünün başlığı
+        public string? Title { get; set; }
+
+        //Skill bölümünün değeri
+        public string? Value { get; set; }
+    }
+}
