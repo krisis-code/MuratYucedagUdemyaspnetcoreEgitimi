a8c089a1e1a43809c4510f75a4339a5c2b71ade2
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-05-29 23:02:50 +0300

    admin teması revize edildi layoutlar ucundan yapıldı çok ağır ilerliyor odağımı veremiyorum ama olsun

---
+++ CvWebSite/CvWebSite/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar() { 
+        
+            return PartialView();
+        
+        }
+
+        public PartialViewResult PartialHead()
+        {
+
+            return PartialView();
+
+        }
+        public PartialViewResult PartialScript()
+        {
+
+            return PartialView();
+
+        }
+    }
+}
---
+++ CvWebSite/CvWebSite/Controllers/SkillController.cs
@@ -0,0 +1,16 @@
+﻿using BusinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.Controllers
+{
+    public class SkillController : Controller
+    {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
+        public IActionResult Index()
+        {
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+    }
+}
