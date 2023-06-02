0b8969fe07b7a4c13b8482205a2ed7b80df50e4c
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-06-02 23:25:56 +0300

    Tema değişikliği yeni tema dosyaları eklendi

---
+++ CvWebSite/CvWebSite/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
