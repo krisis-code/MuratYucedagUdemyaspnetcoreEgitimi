7553ae4e45ba97304ddfa16c252885788979ba9c
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-06-02 22:37:22 +0300

    Hizmetlerim kısmı ekleme , silme ve güncelleme işlemi  eklendi

--- CvWebSite/CvWebSite/Controllers/ServiceController.cs
+++ CvWebSite/CvWebSite/Controllers/ServiceController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinesLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CvWebSite.Controllers
@@ -8,12 +9,61 @@ namespace CvWebSite.Controllers
     {
         ServiceManager serviceManager = new ServiceManager(new EfServiceDall());
 
-        [HttpGet]
+       
         public IActionResult Index()
         {
-;           var values = serviceManager.TGetList();
+            ViewBag.v1 = "Servis Listesi";
+            ViewBag.v2 = "Servis";
+            ViewBag.v3 = "Servis Listesi";
+           var values = serviceManager.TGetList();
 
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddService()
+        {
+
+            ViewBag.v1 = "Servis Ekleme";
+            ViewBag.v2 = "Servis";
+            ViewBag.v3 = "Servis Ekleme";
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult AddService(Service service)
+        {
+            serviceManager.TAdd(service);
+            return RedirectToAction("Index");
+
+        }
+        public IActionResult DeleteService(int id)
+        {
+            var values = serviceManager.TGetById(id);
+            serviceManager.TDelete(values);
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public IActionResult EditService(int id)
+        {
+            ViewBag.v1 = "Yetenek Düzenleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Düzenleme";
+            var values = serviceManager.TGetById(id);
+
+            return View(values);
+
+        }
+        [HttpPost]
+        public IActionResult EditService(Service service)
+        {
+            ViewBag.v1 = "Yetenek Düzenleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Düzenleme";
+            serviceManager.TUpdate(service);
+
+            return RedirectToAction("Index");
+
         }
     }
 }
