b8d9d16abee4144d53aefc67efed5ed4a90dae43
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-06-02 21:47:08 +0300

    Hakkında veri güncelleme eklendi

---
+++ CvWebSite/CvWebSite/Controllers/AboutController.cs
@@ -0,0 +1,41 @@
+﻿using BusinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.Controllers
+{
+    public class AboutController : Controller
+    {
+
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Hakkında Düzenleme";
+            ViewBag.v2 = "Hakkında";
+            ViewBag.v3 = "Hakkında Düzenleme";
+            var values = aboutManager.TGetById(1);
+
+            return View(values);
+
+
+
+
+
+
+        }
+        [HttpPost]
+        public IActionResult Index(About about)
+        {
+            ViewBag.v1 = "Hakkında Düzenleme";
+            ViewBag.v2 = "Hakkında";
+            ViewBag.v3 = "Hakkında Düzenleme";
+            aboutManager.TUpdate(about);
+
+            return RedirectToAction("Index", "Default");
+
+        }
+
+    }
+}
