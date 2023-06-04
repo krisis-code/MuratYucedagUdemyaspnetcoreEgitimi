24876b7206e836e10c5295541835cad9826f51bd
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-06-04 22:57:08 +0300

    ders 86 bitti css hatasını çözdüm

---
+++ CvWebSite/CvWebSite/ViewComponents/Dashboard/StatisticDasboard2.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.ViewComponents.Dashboard
+{
+    public class StatisticDasboard2 : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = c.portfolios.Count();
+            ViewBag.v2 = c.services.Count();
+            ViewBag.v3 = c.messages.Count();
+            return View();
+
+        }
+
+    }
+}
