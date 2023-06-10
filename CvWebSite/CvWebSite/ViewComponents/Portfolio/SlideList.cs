234779ac2294342cdc35b5cfe725754900b9e949
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-06-10 16:15:24 +0300

    ders 96 bitti slide panel partial view e taşındı ve resim verileri çekildi

---
+++ CvWebSite/CvWebSite/ViewComponents/Portfolio/SlideList.cs
@@ -0,0 +1,17 @@
+﻿using BusinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.ViewComponents.Portfolio
+{
+    public class SlideList : ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = portfolioManager.TGetList();
+
+            return View(values);
+        }
+    }
+}
