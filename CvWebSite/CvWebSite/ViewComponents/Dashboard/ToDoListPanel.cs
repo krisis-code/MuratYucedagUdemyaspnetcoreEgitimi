b2107c1c5c26a7ba3b05842002f3bea0c6911425
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-06-12 23:12:57 +0300

    ders 99 bitti todolist verileri çekildi

---
+++ CvWebSite/CvWebSite/ViewComponents/Dashboard/ToDoListPanel.cs
@@ -0,0 +1,18 @@
+﻿using BusinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.ViewComponents.Dashboard
+{
+    public class ToDoListPanel : ViewComponent
+    {
+        TodoListManager todoListManager = new TodoListManager(new EfTodolistDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = todoListManager.TGetList();
+            return View(values);
+
+        }
+    }
+}
