756a56c19f1526c2a32568ebe2b2fe90159a7296
krisis-code <menesbaspinar@hotmail.com>
2023-06-14 15:00:48 +0300

    Ders 99 bitti Dasboard üstündeki bütün bölümler dolduruldu

---
+++ CvWebSite/CvWebSite/ViewComponents/Dashboard/VisitorMap.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.ViewComponents.Dashboard
+{
+    public class VisitorMap : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
