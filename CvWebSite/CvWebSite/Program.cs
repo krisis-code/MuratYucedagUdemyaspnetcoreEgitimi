6d06cd52e779585066708157bbd517d5d671ce53
krisis-code <menesbaspinar@hotmail.com>
2023-06-15 12:12:24 +0300

    Ders 102 bitti writer layout oluşturuldu

--- CvWebSite/CvWebSite/Program.cs
+++ CvWebSite/CvWebSite/Program.cs
@@ -24,11 +24,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
+
+    app.MapControllerRoute(
       name: "areas",
       pattern: "{area:exists}/{controller=Default}/{action=Index}/{id?}"
     );
-});
+
 app.Run();
