34be03c74cba7d3da05a2b3c0202364b841bc164
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-06-18 11:25:40 +0300

    Ders 105 bitti register controller ve view i eklendi

---
+++ CvWebSite/CvWebSite/Areas/Writer/Controllers/RegisterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.Areas.Writer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [Area("Writer")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+    }
+}
