d254e652014f16ff6e4c3dbb880fe728c26648bf
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-06-18 18:16:04 +0300

    ders 113 bitti register sayfa tasarımı bitti

--- CvWebSite/CvWebSite/Areas/Writer/Controllers/RegisterController.cs
+++ CvWebSite/CvWebSite/Areas/Writer/Controllers/RegisterController.cs
@@ -1,26 +1,62 @@
 ﻿using CvWebSite.Areas.Writer.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CvWebSite.Areas.Writer.Controllers
 {
     public class RegisterController : Controller
     {
+
+
+        //private readonly UserManager<WriterUser> _userManager;
+
+        //public RegisterController(UserManager<WriterUser> userManager)
+        //{
+        //    _userManager = userManager;
+        //}
+
         [Area("Writer")]
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(new UserRegisterViewModel());
         }
 
         [HttpPost]
-        public IActionResult Index(UserRegisterViewModel p)
+        public async Task< IActionResult> Index(UserRegisterViewModel p)
         {
-            if (ModelState.IsValid)
-            {
-                
+            //if (ModelState.IsValid)
+            //{
+            //    WriterUser w = new WriterUser()
+            //    {
+            //        Name = p.Name,
+            //        Surname = p.Surname,
+            //        Email = p.Mail,
+            //        UserName = p.UserName,
+            //        ImageUrl = p.ImageUrl
 
-            }
-            return View();
+            //    };
+
+            //    var result = await _userManager.CreateAsync(w,p.Password);
+
+            //    if (result.Succeeded)
+            //    {
+            //        return RedirectToAction("/Index", "Login");
+            //    }
+            //    else
+            //    {
+            //        foreach (var item in result.Errors)
+            //        {
+            //            ModelState.AddModelError("", item.Description);
+
+            //        }
+
+            //    }
+             
+            //}
+        
+            return View(p);
         }
     }
 }
--- CvWebSite/EntityLayer/Concrete/WriterUser.cs
+++ CvWebSite/EntityLayer/Concrete/WriterUser.cs
@@ -7,7 +7,7 @@ namespace EntityLayer.Concrete
     {
         public string Name { get; set; }
 
-        public string Surename { get; set; }
+        public string Surname { get; set; }
         public string ImageUrl { get; set; }
 
 
