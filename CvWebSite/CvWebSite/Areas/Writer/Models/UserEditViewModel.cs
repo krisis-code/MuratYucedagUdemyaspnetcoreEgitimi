acaa5ec4be1c2a888c8d13e0b9c4e9fac1e48581
krisis-code <menesbaspinar@hotmail.com>
2023-06-22 10:56:01 +0300

    Ders 131 bitti user control update işlemleri tanımlandı

--- CvWebSite/CvWebSite/Areas/Writer/Models/UserEditViewModel.cs
+++ CvWebSite/CvWebSite/Areas/Writer/Models/UserEditViewModel.cs
@@ -2,5 +2,16 @@
 {
     public class UserEditViewModel
     {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Password { get; set; }
+
+        public string PasswordConfirm { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public IFormFile Picture { get; set; }
     }
 }
