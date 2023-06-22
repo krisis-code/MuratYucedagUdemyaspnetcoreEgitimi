6a800413a3023ed36c27788d4ca8e1af02d9e7c9
krisis-code <menesbaspinar@hotmail.com>
2023-06-22 10:11:27 +0300

    Ders 128 bitti profil düzenleme ekranı bilgi çekme işlemi yapıldı

--- CvWebSite/CvWebSite/Areas/Writer/Controllers/ProfileController.cs
+++ CvWebSite/CvWebSite/Areas/Writer/Controllers/ProfileController.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CvWebSite.Areas.Writer.Controllers
 {
     [Area("Writer")]
     public class ProfileController : Controller
     {
-        
-        public IActionResult Index()
+        private readonly UserManager<WriterUser> _userManager;
+
+        public ProfileController(UserManager<WriterUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            return View(values);
         }
     }
 }
