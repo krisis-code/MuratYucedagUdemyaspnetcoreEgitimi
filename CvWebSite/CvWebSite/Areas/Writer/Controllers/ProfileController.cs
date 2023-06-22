f0b909a7c9ba6fc5bfabc8b68c174e95695251cf
krisis-code <menesbaspinar@hotmail.com>
2023-06-22 16:42:17 +0300

    Ders 132 bitti update işlemri yapıldı

--- CvWebSite/CvWebSite/Areas/Writer/Controllers/ProfileController.cs
+++ CvWebSite/CvWebSite/Areas/Writer/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
-﻿using EntityLayer.Concrete;
+﻿using CvWebSite.Areas.Writer.Models;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,12 +14,40 @@ namespace CvWebSite.Areas.Writer.Controllers
         {
             _userManager = userManager;
         }
-
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
 
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
-            return View(values);
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name = values.Name;
+            model.Surname = values.Surname;
+            model.PictureUrl = values.ImageUrl;
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Picture != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(p.Picture.FileName);
+                var ImageName = Guid.NewGuid() + extension;
+                var SaveLocations = resource + "/wwwroot/UserImage/" + ImageName;
+                var stream = new FileStream(SaveLocations, FileMode.Create);
+                await p.Picture.CopyToAsync(stream);
+                user.ImageUrl = ImageName;
+            }
+            user.Name = p.Name;
+            user.Surname = p.Surname;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
         }
     }
 }
