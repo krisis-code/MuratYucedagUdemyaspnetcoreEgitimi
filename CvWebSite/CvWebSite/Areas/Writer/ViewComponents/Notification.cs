5a2aa36d67b2d3c366054ea7bf5ec2cb8369c463
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-06-27 21:08:00 +0300

    ders 140 bitti bildirimler listelendi

--- CvWebSite/CvWebSite/Areas/Writer/ViewComponents/Notification.cs
+++ CvWebSite/CvWebSite/Areas/Writer/ViewComponents/Notification.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CvWebSite.Areas.Writer.ViewComponents
 {
     public class Notification : ViewComponent
     {
-
+        AnnouncementsManager announcementsManager = new AnnouncementsManager(new EfannouncementsDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = announcementsManager.TGetList().Take(5).ToList();
+            return View(values);
         }
     }
 }
