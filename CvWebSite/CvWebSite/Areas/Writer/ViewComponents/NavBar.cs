07752200ffcef318a25113f36164c261d04a60dc
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-06-27 22:14:42 +0300

    ders 141 bitti oturum açan kişi resmi çekildi

---
+++ CvWebSite/CvWebSite/Areas/Writer/ViewComponents/NavBar.cs
@@ -0,0 +1,25 @@
+﻿using BusinesLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.Areas.Writer.ViewComponents
+{
+    public class NavBar : ViewComponent
+    {
+
+        private readonly UserManager<WriterUser> _userManager;
+
+        public NavBar(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.ImageUrl;
+            return View(values);
+        }
+    }
+}
