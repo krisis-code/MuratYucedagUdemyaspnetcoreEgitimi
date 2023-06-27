612f3e5bbdaacde3b5f667d5cc968e72e462fc1b
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-06-27 18:49:30 +0300

    ders 139 bitti layout bildirim ayarları yapıldı

--- CvWebSite/DataAccessLayer/Concrete/Migrations/ContextModelSnapshot.cs
+++ CvWebSite/DataAccessLayer/Concrete/Migrations/ContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace DataAccessLayer.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.5")
+                .HasAnnotation("ProductVersion", "7.0.8")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
