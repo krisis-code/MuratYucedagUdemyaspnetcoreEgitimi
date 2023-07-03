c7cd232ad20d4a1780000b1b206d12b6f297f47d
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-07-03 21:03:36 +0300

    Ders 155 bitti mesaj gönderir ken alıcı ismi database e ekleme işlemi eklendi

--- CvWebSite/CvWebSite/Areas/Writer/Controllers/MessageController.cs
+++ CvWebSite/CvWebSite/Areas/Writer/Controllers/MessageController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinesLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
@@ -62,6 +63,9 @@ namespace CvWebSite.Areas.Writer.Controllers
             p.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
             p.Sender = mail;
             p.SenderName = name;
+            Context c = new Context();
+            var usernamesurname = c.Users.Where(x => x.Email ==p.Receiver).Select(y=> y.Name + " " +y.Surname).FirstOrDefault();
+            p.ReceiverName = usernamesurname;
             writerMessageManager.TAdd(p);
             return RedirectToAction("SenderMessage","Message");
         }
