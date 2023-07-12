299176254dc9e3b6f608edfdd7496bf977f3dc63
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-07-12 23:12:21 +0300

    ders 179 , 180, 181 bitti sosyal medya ekleme silme listeleme güncelleme işlemleri tamam

---
+++ CvWebSite/CvWebSite/Controllers/SocialMediaController.cs
@@ -0,0 +1,55 @@
+﻿using BusinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        SocialMediaManager socialMediaManager = new SocialMediaManager(new EfSocialMediaDal());
+        public IActionResult Index()
+        {
+            var values = socialMediaManager.GetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSocialMedia() 
+        {
+            return View();
+                
+        }
+        [HttpPost]
+        public IActionResult AddSocialMedia(SocialMedia p)
+        {
+            socialMediaManager.Insert(p);
+                
+            return RedirectToAction("Index");
+
+        }
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var values = socialMediaManager.GetById(id);
+            socialMediaManager.Delete(values);
+
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public IActionResult EditSocialMedia(int id)
+        {
+            var values = socialMediaManager.GetById(id);
+            return View(values);
+
+        }
+        [HttpPost]
+        public IActionResult EditSocialMedia(SocialMedia p)
+        {
+        
+            socialMediaManager.Update(p);
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
