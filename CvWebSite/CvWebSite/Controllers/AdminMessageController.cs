f10f33125231ef85583a9981c16c0fe985a49d18
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-07-12 20:07:09 +0300

    Nuget paketleri güncdellendi

---
+++ CvWebSite/CvWebSite/Controllers/AdminMessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.Controllers
+{
+    public class AdminMessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
