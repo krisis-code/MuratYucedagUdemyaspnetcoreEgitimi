9c7cd8c8f8ec47ce13a9886683bf1ae693af0ccd
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-07-13 00:39:32 +0300

    Ders 182 ve 183 bitti

---
+++ CvWebSite/CvWebSite/Models/ViewComponents/SocialMedia/SocialMediaList.cs
@@ -0,0 +1,21 @@
+﻿using BusinesLayer.Concrete;
+
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.ViewComponents.SocialMedia
+{
+    public class SocialMediaList:ViewComponent
+    {
+        SocialMediaManager socialMedia = new SocialMediaManager(new EfSocialMediaDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = socialMedia.GetList();
+            return View(values);
+        }
+    }
+}
