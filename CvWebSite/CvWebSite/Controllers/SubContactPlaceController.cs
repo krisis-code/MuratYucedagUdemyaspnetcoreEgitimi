1040c48b31b032686e695c8ac1570f563aa8bccd
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-07-12 23:01:05 +0300

    ders 178 ve 179 bitti testimonal details ve subcontact place bitti

---
+++ CvWebSite/CvWebSite/Controllers/SubContactPlaceController.cs
@@ -0,0 +1,27 @@
+﻿using BusinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.Controllers
+{
+    public class SubContactPlaceController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var values = contactManager.TGetById(1);
+            return View(values);
+
+        }
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+
+            contactManager.TUpdate(contact);
+            return RedirectToAction("Index", "Default");
+
+        }
+    }
+}
