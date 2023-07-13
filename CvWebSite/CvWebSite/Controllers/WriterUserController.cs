37087e85114ce5ace10f29970dddcb680c4ded03
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-07-13 23:13:07 +0300

    ders 198 bitti

--- CvWebSite/CvWebSite/Controllers/WriterUserController.cs
+++ CvWebSite/CvWebSite/Controllers/WriterUserController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CvWebSite.Controllers
 {
     public class WriterUserController : Controller
     {
+        WriterUserManager userManager = new WriterUserManager(new EfWriterUserDal());
         public IActionResult Index()
         {
+           
             return View();
+        }
+        public IActionResult ListUser() 
+        {
+            var values = JsonConvert.SerializeObject(userManager.TGetList());
+            return Json(values);
+
         }
     }
 }
