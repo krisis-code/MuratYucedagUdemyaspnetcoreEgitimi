518aeb432698910696f2e6cc50fdeec4446f4f4c
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-07-13 19:55:11 +0300

    Ders 186 bitti

--- CvWebSite/CvWebSite/Models/ViewComponents/Dashboard/AdminNavbarMessageList.cs
+++ CvWebSite/CvWebSite/Models/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CvWebSite.Models.ViewComponents.Dashboard
 {
     public class AdminNavbarMessageList : ViewComponent
     {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            string p = "admin@gmail.com";
+            var values = writerMessageManager.GetListReceiverMessage(p).OrderByDescending(x => x.WriterMessageId).Take(3).ToList();
+            return View(values);
         }
     }
 }
