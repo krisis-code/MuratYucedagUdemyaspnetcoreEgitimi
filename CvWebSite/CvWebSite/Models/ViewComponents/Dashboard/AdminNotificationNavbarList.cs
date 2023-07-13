e1de4f69aaefb650e94aeb6449137c003fca75d5
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-07-13 20:15:39 +0300

    Ders 187 bitti

---
+++ CvWebSite/CvWebSite/Models/ViewComponents/Dashboard/AdminNotificationNavbarList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.Models.ViewComponents.Dashboard
+{
+    public class AdminNotificationNavbarList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
