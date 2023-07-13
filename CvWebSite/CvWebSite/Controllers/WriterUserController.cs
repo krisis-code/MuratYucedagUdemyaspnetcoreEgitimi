3d00fe586814ed7e7538a4c61fac8746af12d8fc
Enes Başpınar <103113392+krisis-code@users.noreply.github.com>
2023-07-13 23:01:24 +0300

    ders 196,197 bitti

---
+++ CvWebSite/CvWebSite/Controllers/WriterUserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.Controllers
+{
+    public class WriterUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
