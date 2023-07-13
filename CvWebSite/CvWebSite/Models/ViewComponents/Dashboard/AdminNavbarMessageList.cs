dded0f01147a656bd1b7daa4f88c3e26f0cf8dcb
krisis-code <menesbaspinar@hotmail.com>
2023-07-13 12:22:21 +0300

    ders 185 bitti üst menü tasarımı yapıldı

---
+++ CvWebSite/CvWebSite/Models/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CvWebSite.Models.ViewComponents.Dashboard
+{
+    public class AdminNavbarMessageList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
